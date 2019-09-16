d6f9317b18464e9b5b4040e5a728432a4306066e
Nohelia Cristina Euceda Figueroa <neuceda@HN-HNSC00396M.loe.corp>
2019-09-15 19:17:11 -0600

    playing with json

--- src/minipascal/Main.java
+++ src/minipascal/Main.java
@@ -3,14 +3,21 @@ package minipascal;
 import minipascal.Lexer.Lexer;
 import minipascal.Parser.parser;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import java.io.FileReader;
 import java.io.StringReader;
 
-import java_cup.runtime.ComplexSymbolFactory;
-import java_cup.runtime.ScannerBuffer;
-import java_cup.runtime.XMLElement;
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java_cup.runtime.*;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.Reader;
+
 import java.io.*;
 
 import javax.xml.transform.*;
@@ -20,9 +27,29 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            ScannerBuffer lexer = new ScannerBuffer(new Lexer((new FileReader("inputs/rel.pas"))));
-            parser p = new parser(lexer);
-            //XMLElement e = (XMLElement)p.parse().value;
+            Reader reader;
+            try {
+                reader = new BufferedReader(new FileReader("inputs/rel.pas"));
+                Lexer lexer = new Lexer(reader);
+
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+                mapper.setVisibilityChecker(mapper.getSerializationConfig().getDefaultVisibilityChecker()
+                        .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+                        .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+                        .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+                        .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+                mapper.setSerializationInclusion(Include.NON_NULL);
+                //mapper.setVisibility(JsonMethod.FIELD, Visibility.ANY);
+
+                parser cupParser = new parser(lexer);
+                cupParser.parse();
+                mapper.writeValue(new File("AST.json"), cupParser.root);
+
+
+            } catch (FileNotFoundException ex) {
+                System.out.println(ex);
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
